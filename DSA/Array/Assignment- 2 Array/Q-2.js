e2d44d6fb3ac3093678f83be51850ac77bb37794
Rishabh Srivastava <121332802+Rishabh6306@users.noreply.github.com>
2023-10-02 20:59:24 +0530

    Q-2.js

--- DSA/Array/Assignment- 2 Array/Q-2.js
+++ DSA/Array/Assignment- 2 Array/Q-2.js
@@ -10,7 +10,7 @@
 // Output: 3
 
 // Explanation: Alice can only eat 6 / 2 = 3 candies. Since there are only 3 types, she can eat one of each type.
-
+ 
 const candyType = [1, 1, 2, 2, 3, 3];
 
 function maxCandies(candyType) {
